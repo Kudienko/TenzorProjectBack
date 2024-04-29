83c7b4e94e3b5c22e688ca004df06c4243f20ab3
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-04-29 16:58:42 +0500

    added city list and redis connect

--- src/router.py
+++ src/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from src.config import settings
 from src.http_client import OpenWeatherHTTPClient
-from async_lru import alru_cache
+# from async_lru import alru_cache
 
 from fastapi_users import FastAPIUsers
 
@@ -9,6 +9,13 @@ from auth.auth import auth_backend
 from auth.database import CustomUser
 from auth.manager import get_user_manager
 from auth.schemas import UserRead, UserCreate, UserUpdate
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
+
+import json
 
 router = APIRouter(
     prefix="/api"
@@ -19,9 +26,11 @@ fastapi_users = FastAPIUsers[CustomUser, int](
     [auth_backend],
 )
 
+cache_backend = None
+
 router.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
+    prefix="/auth",
     tags=["auth"],
 )
 
@@ -30,7 +39,6 @@ router.include_router(
     prefix="/auth",
     tags=["auth"],
 )
-
 
 router.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
@@ -45,8 +53,30 @@ def hello():
 
 
 @router.get("/get_weather/{city}")
-@alru_cache
+@cache(expire=120)
 async def get_weather(city: str):
     open_weather_client = OpenWeatherHTTPClient(base_url="https://api.openweathermap.org", city=city,
                                                 api_key=settings.OPEN_WEATHER_KEY)
     return await open_weather_client.get_weather()
+
+
+@router.get("/cities/{city}")
+@cache(expire=120)
+async def get_similar_cities(input_city: str):
+    cities_str = await cache_backend.get("cities")
+    cities = json.loads(cities_str) if cities_str else []
+    search_results = [city for city in cities if input_city.lower() in city["name"].lower()]
+    return search_results
+
+
+# Загрузка списка городов в redis при старте сервера
+@router.on_event("startup")
+async def load_cities():
+    global cache_backend
+    redis = aioredis.from_url("redis://localhost")
+    cache_backend = RedisBackend(redis)
+    FastAPICache.init(cache_backend, prefix="fastapi-cache")
+    with open('cities.json', 'r', encoding='utf-8') as f:
+        city_data = json.load(f)
+        cities = city_data.get("city", [])
+        await cache_backend.set("cities", json.dumps(cities))
